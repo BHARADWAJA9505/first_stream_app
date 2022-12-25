6175ace96abb2bf571bb116900d40627453e01de
CHANDULA V SAI BHARADWAJA REDDDY <55663485+BHARADWAJA9505@users.noreply.github.com>
2022-12-25 21:47:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+fruit_choi
 
 
 import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
