909fb8344b5899610f8066bc22aff9aee7e1b9a3
CHANDULA V SAI BHARADWAJA REDDDY <55663485+BHARADWAJA9505@users.noreply.github.com>
2022-12-26 09:42:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,29 +44,35 @@ try:
 except URLError as e:
     streamlit.error()
 #import requests
-
-
-
-
-streamlit.stop();
-
 #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
-
-
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+def get_fruit_load_list():
+      with  my_cnx.cursor() as my_cur:
+            my_cur.execute("SELECT * FROM fruit_load_list")
+            return my_cur.fetchall()
+if streamlit.buttom('get fruit load list'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.text(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
-streamlit.write('The user entered ', add_my_fruit)
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#
+#streamlit.text("Hello from Snowflake:")
+#
+#my_data_rows = my_cur.fetchall()
+#streamlit.dataframe(my_data_rows)
+
+def insert_row_snowflake(new_fruit):
+      with  my_cnx.cursor() as my_cur:
+            my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+            return "Thanks for adding" +new_fruit
+            
+add_my_fruit = streamlit.text_input('What fruit would you like to add?') 
+if streamlit.buttom('Add a fruit to the list'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      back_from_function = insert_row_snowflake(add_my_fruit)
+      streamlit.text(back_from_function)
 
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
